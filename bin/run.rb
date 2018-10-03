49dcb31794ebefffa01cd55a805834d7abe27af3
tomgiagtz <tom.giagtzoglou@gmail.com>
2018-10-03 14:24:31 -0400

    add highline gem, and set up method header in runner.rb

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,6 @@
 require_relative '../config/environment'
+require_relative './runner.rb'
 
-
+start
 
 puts "HELLO WORLD"
---
+++ bin/runner.rb
@@ -0,0 +1,34 @@
+#runner file for solar pay
+require 'highline'
+
+@cli = HighLine.new
+
+
+def welcome
+	@cli.say("Welcome to:")
+	@cli.say("SOLAR PAY")
+end
+
+def start
+	welcome
+end
+
+def get_user_data
+
+end
+
+def get_name
+
+end
+
+def get_zipcode
+
+end
+
+def get_state
+
+end
+
+def get_bills
+
+end
